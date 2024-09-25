cc0bd4976997f735f88d2e04788c7d800bf4be80
Sara <123300249+sarav929@users.noreply.github.com>
2024-09-25 15:19:58 +0100

    wrap game functions in Tictactow module

--- script.js
+++ script.js
@@ -2,22 +2,10 @@
 // Your players are also going to be stored in objects
 // an object to control the flow of the game itself
 
-const WIN_COMBOS = [ [0,1,2], [3,4,5], [6,7,8], 
-[0, 3, 6], [1, 4, 7], [2, 5, 8], 
-[0, 4, 8], [2, 4, 6]]
-
 function Game() {
     this.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     this.turn = 1
     this.winner = ""
-}
-
-function displayBoard(board) {
-    console.log(`${board[0]} | ${board[1]} | ${board[2]}`)
-    console.log("----------")
-    console.log(`${board[3]} | ${board[4]} | ${board[5]}`)
-    console.log("----------")
-    console.log(`${board[6]} | ${board[7]} | ${board[8]}`)
 }
 
 function Player(name, symbol, score = 0) {
@@ -25,48 +13,69 @@ function Player(name, symbol, score = 0) {
     this.score = score
     this.symbol = symbol
 }
+    
+const ticTacToe = (function() {
 
-function makeMove(game, player1, player2) {
+    const displayBoard = (board) => {
+        console.log(`${board[0]} | ${board[1]} | ${board[2]}`)
+        console.log("----------")
+        console.log(`${board[3]} | ${board[4]} | ${board[5]}`)
+        console.log("----------")
+        console.log(`${board[6]} | ${board[7]} | ${board[8]}`)
+    }
 
-    if (game.turn % 2 != 0) {
-        var currentPlayer = player1
-    } else {
-        currentPlayer = player2
+    const makeMove = (game, player1, player2) => {
+
+        if (game.turn % 2 != 0) {
+            var currentPlayer = player1
+        } else {
+            currentPlayer = player2
+        }
+        
+        let move = window.prompt(`${currentPlayer.name}: what's your next move?`)
+    
+        game.board[parseInt(move) - 1] = currentPlayer.symbol
+        game.turn++
+    
+        ticTacToe.checkWin(game)
+        ticTacToe.displayBoard(game.board)
     }
     
-    let move = window.prompt(`${currentPlayer.name}: what's your next move?`)
+    const checkWin = (game) => {
 
-    game.board[parseInt(move) - 1] = currentPlayer.symbol
-    game.turn++
+        const WIN_COMBOS = [[0,1,2], [3,4,5], [6,7,8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]]
+    
+        WIN_COMBOS.forEach((combo) => {
+            let subString = [game.board[combo[0]], game.board[combo[1]], game.board[combo[2]]]
+            if (subString.every((el) => el == "o")) {
+                game.winner = player1.name
+                console.log(`Winner is ${game.winner}`)
+            } else if (subString.every((el) => el == "x")) {
+                game.winner = player2.name
+                console.log(`Winner is ${game.winner}`)
+            }       
+        })
+    }
 
-    checkWin(game)
-    displayBoard(game.board)
-}
+    const playGame = (game, player1, player2) => {
+        while (game.winner == "") {
+            ticTacToe.makeMove(game, player1, player2)
+        }
+    }
 
-function checkWin(game) {
-
-    WIN_COMBOS.forEach((combo) => {
-        let subString = [game.board[combo[0]], game.board[combo[1]], game.board[combo[2]]]
-        if (subString.every((el) => el == "o")) {
-            game.winner = player1.name
-            console.log(`Winner is ${game.winner}`)
-        } else if (subString.every((el) => el == "x")) {
-            game.winner = player2.name
-            console.log(`Winner is ${game.winner}`)
-        }       
-    })
-}
+    return {displayBoard, makeMove, checkWin, playGame}
+})()
 
 player1 = new Player("ME", "o")
 player2 = new Player("YOU", "x")
-
 game = new Game
 
-displayBoard(game.board)
+ticTacToe.playGame(game, player1, player2)
 
-while (game.winner == "") {
-    makeMove(game, player1, player2)
-}
+
+
 
 
 
