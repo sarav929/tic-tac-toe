fb0b190297164ddea876a6b1d0a5005b77359870
Sara <123300249+sarav929@users.noreply.github.com>
2024-09-25 11:39:13 +0100

    add function to check winner after move add fucntion to display board in console

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 // Your players are also going to be stored in objects
 // an object to control the flow of the game itself
 
+const WIN_COMBOS = [ [0,1,2], [3,4,5], [6,7,8], 
+[0, 3, 6], [1, 4, 7], [2, 5, 8], 
+[0, 4, 8], [2, 4, 6]]
+
 function Game() {
-    this.board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ]
+    this.board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     this.turn = 1
     this.winner = ""
+}
+
+function displayBoard(board) {
+    console.log(`${board[0]} | ${board[1]} | ${board[2]}`)
+    console.log("----------")
+    console.log(`${board[3]} | ${board[4]} | ${board[5]}`)
+    console.log("----------")
+    console.log(`${board[6]} | ${board[7]} | ${board[8]}`)
 }
 
 function Player(name, symbol, score = 0) {
@@ -26,29 +34,39 @@ function makeMove(game, player1, player2) {
         currentPlayer = player2
     }
     
-    let move = window.prompt(`${currentPlayer.name}: what's your next move?`).split(" ")
+    let move = window.prompt(`${currentPlayer.name}: what's your next move?`)
 
-    let row = parseInt(move[0]) - 1
-    let col = parseInt(move[1]) - 1
-
-    game.board[row][col] = currentPlayer.symbol
+    game.board[parseInt(move) - 1] = currentPlayer.symbol
     game.turn++
 
+    checkWin(game)
+    displayBoard(game.board)
 }
 
+function checkWin(game) {
 
-player1 = new Player("me", "o")
-player2 = new Player("you", "x")
+    WIN_COMBOS.forEach((combo) => {
+        let subString = [game.board[combo[0]], game.board[combo[1]], game.board[combo[2]]]
+        if (subString.every((el) => el == "o")) {
+            game.winner = player1.name
+            console.log(`Winner is ${game.winner}`)
+        } else if (subString.every((el) => el == "x")) {
+            game.winner = player2.name
+            console.log(`Winner is ${game.winner}`)
+        }       
+    })
+}
+
+player1 = new Player("ME", "o")
+player2 = new Player("YOU", "x")
 
 game = new Game
 
-console.log(game)
+displayBoard(game.board)
 
-makeMove(game, player1, player2)
-makeMove(game, player1, player2)
-makeMove(game, player1, player2)
-makeMove(game, player1, player2)
-makeMove(game, player1, player2)
+while (game.winner == "") {
+    makeMove(game, player1, player2)
+}
 
 
 
